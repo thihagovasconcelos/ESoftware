1193b025b826f6b838eaac01bffb702f79866adf
ewerton.guedes <ewerton.guedes@LAB2-DCX-22.dcx.ufpb.br>
2017-09-11 11:41:18 -0300

    Editei

--- AulaEng.Software/src/Thiago.java
+++ AulaEng.Software/src/Thiago.java
@@ -3,5 +3,6 @@ public class Thiago {
 	public static void main(String[] args) {  
 	     System.out.println("Aula Eng.Software"); 
 	     System.out.println("Editei o teu(Luandersan)"); 
+	     System.out.println("Ewerton editou");
 	   }  
 }
